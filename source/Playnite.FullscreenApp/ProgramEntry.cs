c8f228138f356ab28743f48551b30297b7f11e8e
ashpynov <ashpynov@gmail.com>
2026-01-02 18:14:08 +0300

    10.47.1

--- source/Playnite.FullscreenApp/ProgramEntry.cs
+++ source/Playnite.FullscreenApp/ProgramEntry.cs
@@ -1,5 +1,6 @@
 ﻿using CommandLine;
 using Playnite.Common;
+using Playnite.FullscreenApp.Windows;
 using Playnite.SDK;
 using System;
 using System.Collections.Generic;
@@ -76,11 +77,11 @@ namespace Playnite.FullscreenApp
                 return;
             }
 
-            SplashScreen splash = null;
+            ExtendedSplashScreen splash = null;
             var procCount = Process.GetProcesses().Where(a => PlayniteApplication.IsProcessPlayniteProcess(a)).Count();
             if (cmdLine.Start.IsNullOrEmpty() && !cmdLine.HideSplashScreen && procCount == 1)
             {
-                splash = new SplashScreen("SplashScreen.png");
+                splash = new ExtendedSplashScreen("SplashScreen.png");
                 splash.Show(false);
             }
 
